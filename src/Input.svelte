c8e698f438c0e4849b0f92a7789ceaa36004d2f6
Javier Reyes <javier.reyes@mercadolibre.com.co>
2021-07-23 01:28:31 -0500

    Conditional rendering with fecthing

--- src/Input.svelte
+++ src/Input.svelte
@@ -1,19 +1,44 @@
 <script>
-    let value = '';
-    let response = [];
+  let value = '';
+  let loading = false
+  let response = [];
 
-    const handleInput = (event) => value = event.target.value;
+  const handleInput = (event) => value = event.target.value;
 
-    $: if (value.length > 3) {
-        fetch(`https://www.omdbapi.com/?s=${value}&apikey=422350ff`)
-            .then(res => res.json())
-            .then(apiResponse => {
-                response = apiResponse.Search;
-                console.log(apiResponse)
-                console.log(response)
-
-            })
+  $: if (value.length > 3) {
+    loading = true
+    
+    fetch(`https://www.omdbapi.com/?s=${value}&apikey=422350ff`)
+      .then(res => res.json())
+      .then(apiResponse => {
+        response = apiResponse.Search || []
+        loading = false
+      })
     }
 </script>
 
-<input value={value} on:input={handleInput} />
+<input 
+    placeholder="Search movies..." 
+    value={value} 
+    on:input={handleInput} 
+/>
+
+{#if loading}
+  <strong>Loading....</strong>
+{:else}
+  {#if response.length > 0}
+    <!--Forma correcta de render HTML en Svelte-->
+    <strong>Tenemos {response.length} peliculas</strong>
+  {:else}
+    <strong>No hay resultados</strong>
+  {/if}
+{/if}
+
+
+<!-- {
+  Forma incorrecta de render Svelte
+    response.length > 0 
+        ? `Tenemos ${response.length} peliculas` 
+        : `No se han encontrado peliculas`
+} -->
+
