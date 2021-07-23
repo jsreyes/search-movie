5da13916e86b7f0fdeb7c7a6098215ca7b0389c7
Javier Reyes <javier.reyes@mercadolibre.com.co>
2021-07-23 00:21:46 -0500

    Handling state and create counter component

---
+++ src/Counter.svelte
@@ -0,0 +1,27 @@
+<script>
+	// Props injection
+	export let initialCounter = 0;
+	let contador = initialCounter
+
+	function handleClick() {
+		contador++;
+	}
+</script>
+
+<main>
+	<button on:click={handleClick}>Incrementar</button>
+	<span>{contador}</span>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	span {
+		color: #ff3e00;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import App from './App.svelte';
 
 const app = new App({
 	target: document.body, // Punto de anclaje para renderizar app Svelte, Donde se quiere renderizar
-	props: {
-		name: 'world'
-	}
 });
 
 export default app;
