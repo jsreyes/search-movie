01d88a18b881d551986ad2b9bb422bf45fcc3b1a
Javier Reyes <javier.reyes@mercadolibre.com.co>
2021-07-23 00:55:42 -0500

    Input component added and fetch to search movies

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,16 @@
 <script>
-	import Counter from "./Counter.svelte";
+	//  import Counter from "./Counter.svelte";
 	import Input from "./Input.svelte";
 
 	const commonProps = { maxCounter: 5}
 </script>
 
-<Counter initialCounter={2} {...commonProps} />
-<Counter initialCounter={5} {...commonProps} />
-<Counter {...commonProps} /> 
+<!-- Reactive declarations 
+	<Counter initialCounter={2} {...commonProps} />
+	<Counter initialCounter={5} {...commonProps} />
+	<Counter {...commonProps} /> 
 
-<span>Estos son los contadores</span>
+	<span>Estos son los contadores</span> 
+-->
+
+<Input />
--- src/Input.svelte
+++ src/Input.svelte
@@ -1,7 +1,19 @@
 <script>
     let value = '';
+    let response = [];
 
     const handleInput = (event) => value = event.target.value;
+
+    $: if (value.length > 3) {
+        fetch(`https://www.omdbapi.com/?s=${value}&apikey=422350ff`)
+            .then(res => res.json())
+            .then(apiResponse => {
+                response = apiResponse.Search;
+                console.log(apiResponse)
+                console.log(response)
+
+            })
+    }
 </script>
 
 <input value={value} on:input={handleInput} />
